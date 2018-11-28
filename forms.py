4efb52731ddcfb6487f3ecb98f5fbd42e00466b6
nrsmoll <nrsmoll@gmail.com>
2018-11-28 22:19:21 +1100

    First working form, with full fields

--- forms.py
+++ forms.py
@@ -1,8 +1,12 @@
 from flask_wtf import Form
-from wtforms import StringField, SubmitField
+from wtforms import StringField, SubmitField, IntegerField
 from wtforms.validators import DataRequired
 
 class CpetForm(Form):
     firstname = StringField('First Name', validators=[DataRequired()])
     lastname = StringField('Last Name', validators=[DataRequired()])
+    age = IntegerField('Age (years)', validators=[DataRequired()])
+    bmi = IntegerField('BMI', validators=[DataRequired()])
+    chronotropic = IntegerField('Chronotropic Response (HR)', validators=[DataRequired()])
+    etco2 = IntegerField('End-Tidal CO2', validators=[DataRequired()])
     submit = SubmitField('Classify')
